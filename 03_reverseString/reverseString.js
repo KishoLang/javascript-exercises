7a50fde4c755e5059fc906b5a3898f39d9b3f717
KishoLang <aklangenberg@gmail.com>
2023-10-02 19:40:44 +0200

    03 reverse done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,11 +1,5 @@
 const reverseString = function(string) {
-    let newCharArray = [];
-    let counter = 0;
-    for (let i = string.length - 1; i >= 0; i--) {
-        newCharArray[counter] = string[i]; 
-        counter++;
-    }
-    return newCharArray.join("");
+   return string.split("").reverse().join(""); 
 };
 
 // Do not edit below this line
