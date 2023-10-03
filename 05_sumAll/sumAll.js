8cdd5cf0b9f182e5b78aa10c302b9299af8fabd4
KishoLang <aklangenberg@gmail.com>
2023-10-03 11:31:27 +0200

    05 finished sum

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let min; 
+    let max;
 
+    if (typeof num1 === "number" && typeof num2 === "number") {
+        if (num1 >= 0 && num2 >= 0) {
+            if (num1 > num2) {
+                max = num1;
+                min = num2;
+            } else {
+                max = num2;
+                min = num1;
+            }
+        } else {
+            return "ERROR";
+        }
+    } else {
+        return "ERROR";
+    }
+
+   
+
+    
+
+    let sum = 0;
+    for (let i = min;  i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
