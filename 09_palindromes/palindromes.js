5403dd0bdc8abdbcffae2402bffe64f63ec3ffde
KishoLang <aklangenberg@gmail.com>
2023-10-09 18:48:56 +0200

    palindrome done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // Turn String into lower case string without spaces or symbols
+    const str = string.replace(/[^A-Za-z0-9]+/g, "").toLowerCase();
+    // Return bool result of reversed str vs str
+    return str.split("").reverse().join("") === str;
+    
+    /* 
+    // Check if first char is === last char
+    let result;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === str[(str.length - 1) - i]) {
+            continue;
+        } else {
+            return false;
+        }
+    }
+    return true;
+    */
 
 };
 
