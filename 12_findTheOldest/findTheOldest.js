ca37be5f446c1650a956461ad1befd7c967d61e5
KishoLang <aklangenberg@gmail.com>
2023-10-11 19:29:45 +0200

    findOldest done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldestList = people.sort((a, b) => getAge(a.yearOfBirth, a.yearOfDeath) - getAge(b.yearOfBirth, b.yearOfDeath));
+    return oldestList[oldestList.length - 1];
 };
+
+const getAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+
+    return death - birth;
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
